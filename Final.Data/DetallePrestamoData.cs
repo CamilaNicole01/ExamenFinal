1bae138aa6a01e9a3d6abaa10c5957eb3e001e6f
CamilaNicole01 <camposcamila192@gmail.com>
2022-09-07 11:58:37 -0500

    ProyectoFinal

--- Final.Data/DetallePrestamoData.cs
+++ Final.Data/DetallePrestamoData.cs
@@ -34,15 +34,39 @@ namespace Final.Data
                                 listado.Add(prestamo);
 
                             }
-}
+                       }
+                    }
+                }
+            }
+            return listado;
+
+        }
+    public prestamo BuscarPorId(int id)
+    {
+        Prestamo prestamo = new Prestamo();
+        using (var conexion = new SqlConnection(cadenaConexion))
+        {
+            conexion.Open();
+            using (var comando = new SqlCommand("SELECT * FROM DetallePrestamo WHERE ID = @id", conexion))
+            {
+                comando.Parameters.AddWithValue("@id", id);
+                using (var lector = comando.ExecuteReader())
+                {
+                    if (lector != null && lector.HasRows)
+                    {
+                        prestamo = new Prestamo();
+                        prestamo.ID = int.Parse(lector[0].ToString());
+                        prestamo.IdPrestamo lector[1].ToString();
+                        prestamo.NumeroCuota = int.Parse(lector[2].ToString());
+                        prestamo.ImporteCuota = decimal.Parse(lector[3].ToString());
+                        prestamo.ImporteInteres = decimal.Parse(lector[4].ToString());
+
+                        listado.Add(prestamo);
                     }
                 }
             }
         }
-        return listado;
-
+            return prestamo;
+        }
     }
-    public Cliente BuscarPorId(int id)
-    {
-
-    }
+}
